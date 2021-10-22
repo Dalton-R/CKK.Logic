7a6124782ba566a6166b7ed895473d91e402fb4b
Dalton-R <92953003+Dalton-R@users.noreply.github.com>
2021-10-22 15:02:23 -0600

    Add files via upload

---
+++ CKK.Logic/CKK.Logic/Models/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    class Product
+    {
+        public static int ID;
+        public static string Name;
+        public static decimal Price;
+
+        public static int GetId()
+        {
+            return ID;
+        }
+
+        public static void SetId(int id)
+        {
+            ID = id;
+        }
+
+        public static string GetName()
+        {
+            return Name;
+        }
+
+        public static void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public static decimal GetPrice()
+        {
+            return Price;
+        }
+
+        public static void SetPrice(decimal price)
+        {
+            Price = price;
+        }
+    }
+}
---
+++ CKK.Logic/CKK.Logic/Models/ShoppingCartItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKK.Logic.Models
+{
+    class ShoppingCartItem
+    {
+        public static Product Product;
+        public static int Quantity;
+
+        public ShoppingCartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public static int GetQuantity()
+        {
+            return Quantity;
+        }
+
+        public static void SetQuantity(int quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public static Product GetProduct()
+        {
+            return Product;
+        }
+
+        public static void SetProduct(Product product)
+        {
+            Product = product;
+        }
+    }
+}
