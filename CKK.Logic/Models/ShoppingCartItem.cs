b70081c7e8c4e3eea25634f6a49921f65b9af564
Dalton-R <92953003+Dalton-R@users.noreply.github.com>
2021-10-26 13:56:57 -0600

    Add files via upload

---
+++ CKK.Logic/Models/ShoppingCartItem.cs
@@ -0,0 +1,35 @@
+﻿
+namespace CKK.Logic.Models
+{
+    public class ShoppingCartItem
+    {
+        public Product Product;
+        public int Quantity;
+
+        public ShoppingCartItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public int GetQuantity()
+        {
+            return Quantity;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public Product GetProduct()
+        {
+            return Product;
+        }
+
+        public void SetProduct(Product product)
+        {
+            Product = product;
+        }
+    }
+}
---
+++ CKK.Logic/Models/StoreItem.cs
@@ -0,0 +1,35 @@
+﻿
+namespace CKK.Logic.Models
+{
+    public class StoreItem
+    {
+        public Product Product;
+        public int Quantity;
+
+        public StoreItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+
+        public int GetQuantity()
+        {
+            return Quantity;
+        }
+
+        public void SetQuantity(int quantity)
+        {
+            Quantity = quantity;
+        }
+
+        public Product GetProduct()
+        {
+            return Product;
+        }
+
+        public void SetProduct(Product product)
+        {
+            Product = product;
+        }
+    }
+}
